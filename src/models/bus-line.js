b14c8bdf656ec93156fa741b5a872c42ed299ac6
Janne Vento <jannejvento@gmail.com>
2020-04-13 23:15:54 +0300

    Remove unused methods

--- src/models/bus-line.js
+++ src/models/bus-line.js
@@ -15,26 +15,4 @@ export default class BusLine {
     getStops() {
         return this.stops
     }
-
-    doesStopIn(stopName) {
-        const stopFound = this.stops.find(stop => stop.getName() === stopName);
-        return !!stopFound;
-    }
-
-    getNextStopFrom(stop) {
-        const indexOfStop = this.stops.indexOf(stop);
-        if(indexOfStop > -1 && indexOfStop + 1 <this.stops.length){
-            return this.stops[indexOfStop + 1]
-        }
-        return null;
-    }
-
-    goingFromAToB(firstStop, secondStop) {
-        const firstStopIndex = this.stops.indexOf(firstStop);
-        const secondStopIndex = this.stops.indexOf(secondStop);
-        if(firstStopIndex > -1 && secondStopIndex > -1 && firstStopIndex < secondStopIndex){
-            return true;
-        }
-        return false;
-    }
 }
