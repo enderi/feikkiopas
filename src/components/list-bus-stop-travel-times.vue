dcc3390e87d0e1305c6814f544e41854cfb501b5
Janne Vento <janne.vento@madtrix.io>
2020-04-14 15:34:11 +0300

    UI mod

--- src/components/list-bus-stop-travel-times.vue
+++ src/components/list-bus-stop-travel-times.vue
@@ -2,7 +2,7 @@
 <div>
     <h3>Pysäkkien välien kestot</h3>
     <div v-for="(road, index) in roads" v-bind:key="index">
-        {{road.from.name}} => {{road.to.name}} = {{road.travelTime}} yksikköä
+        {{road.from.name}} - {{road.to.name}} = {{road.travelTime}} yksikköä
 
     </div>
 </div>
